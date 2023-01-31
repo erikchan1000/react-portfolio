137fced0dffd847e713e07826b9ec7abe7869493
Erik Chan <chanerik1000@gmail.com>
2023-01-31 01:38:28 -0800

    fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.scss';
 import Layout from './components/Layout';
-import {Switch, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -9,14 +9,14 @@ import Contact from './components/Contact';
 function App() {
   return (
     <>
-      <Switch>
+      <Routes>
         <Route path="/" exact element={<Layout/>} >
           <Route index element={<Home/>} />
           <Route path="/about" exact element={<About/>} />
           <Route path="/portfolio" exact element={<Contact/>} />
         </Route>
         
-      </Switch>
+      </Routes>
     </>
   );
 }
