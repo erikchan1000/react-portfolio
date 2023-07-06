b85030b0f55b70670df879c7383c54588f906a99
TonyYep <tonychan@ucsb.edu>
2023-07-05 22:28:40 -0700

    update email 0

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -9,6 +9,12 @@ import './index.scss'
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
   const form = useRef()
+  const [contactInfo, setContactInfo] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  })
 
   useEffect(() => {
     setTimeout(() => {
@@ -16,20 +22,38 @@ const Contact = () => {
     }, 3000)
   }, [])
 
+  //send email function that checks that every required field is filled out
   const sendEmail = (e) => {
     e.preventDefault()
-
-    emailjs
-      .sendForm('gmail', 'template_YeJhZkgb', form.current, 'your-token')
-      .then(
-        () => {
-          alert('Message successfully sent!')
-          window.location.reload(false)
-        },
-        () => {
-          alert('Failed to send the message, please try again')
-        }
-      )
+    if (
+      contactInfo.name === '' ||
+      contactInfo.email === '' ||
+      contactInfo.subject === '' ||
+      contactInfo.message === ''
+    ) {
+      alert('Please fill out all fields')
+    } else {
+      //send to chantony@gmail.com
+      emailjs
+        .sendForm(
+          `${process.REACT_APP_EMAIL_SERVICE}`,
+          `${process.REACT_APP_EMAIL_TEMPLATE}`,
+          {
+            from_name: contactInfo.name,
+            from_subject: contactInfo.subject,
+            from_message: contactInfo.message,
+          }
+        )
+        .then(
+          (result) => {
+            alert('Message sent successfully')
+          },
+          (error) => {
+            alert(error.text)
+          }
+        )
+      form.current.reset()
+    }
   }
 
   const idx = useRef(0)
@@ -52,7 +76,11 @@ const Contact = () => {
             <form ref={form} onSubmit={sendEmail}>
               <ul>
                 <li className="half">
-                  <input placeholder="Name" type="text" name="name" required />
+                  <input placeholder="Name" type="text" name="name" required 
+                    onChange={(e) =>
+                      setContactInfo({ ...contactInfo, name: e.target.value })
+                    }
+                  />
                 </li>
                 <li className="half">
                   <input
@@ -60,6 +88,9 @@ const Contact = () => {
                     type="email"
                     name="email"
                     required
+                    onChange={(e) =>
+                      setContactInfo({ ...contactInfo, email: e.target.value })
+                    }
                   />
                 </li>
                 <li>
@@ -68,6 +99,9 @@ const Contact = () => {
                     type="text"
                     name="subject"
                     required
+                    onChange={(e) =>
+                      setContactInfo({ ...contactInfo, subject: e.target.value })
+                    }
                   />
                 </li>
                 <li>
@@ -75,10 +109,16 @@ const Contact = () => {
                     placeholder="Message"
                     name="message"
                     required
+                    onChange={(e) =>
+                      setContactInfo({ ...contactInfo, message: e.target.value })
+                    }
+
                   ></textarea>
                 </li>
                 <li>
-                  <input type="submit" className="flat-button" value="SEND" />
+                  <input type="submit" className="flat-button" value="SEND"
+                    onClick={(e) => sendEmail(e)}
+                  />
                 </li>
               </ul>
             </form>
